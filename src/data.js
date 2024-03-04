557e8293347525a5d4191910fe432c5dbede4863
Fahmi Zain <fahmizain000@gmail.com>
2024-03-04 14:24:20 +0700

    create data.js

---
+++ src/data.js
@@ -0,0 +1,143 @@
+import htmllogo from "./assets/img/htmllogo.png";
+import csslogo from "./assets/img/csslogo.png";
+import jslogo from "./assets/img/jslogo.png";
+import bootstraplogo from "./assets/img/bootstraplogo.png";
+import tailwindlogo from "./assets/img/tailwindlogo.png";
+import reactlogo from "./assets/img/reactlogo.png";
+import vuelogo from "./assets/img/vuelogo.png";
+
+import gitlogo from "./assets/img/gitlogo.png";
+import vscodelogo from "./assets/img/vscodelogo.png";
+import githublogo from "./assets/img/githublogo.png";
+import npmlogo from "./assets/img/npmlogo.png";
+
+import project1 from "./assets/img/project.png"
+import project2 from "./assets/img/project2.png";
+import project3 from "./assets/img/project3.png";
+import project4 from "./assets/img/project4.png";
+
+export const projects = [
+  {
+    id: 1,
+    img: project1,
+    title: "Netflix Clone",
+    description: "dsds dsdsdsa dsdads adsdsad dsada dsadsa dasd asdasd asdsadsa da"
+  },
+  {
+    id: 2,
+    img: project2,
+    title: "Weather App",
+    description: "ssssssssssssssssssssssssssssss sssssssssssssssssssss sssssssssssssssssss sssssssssss"
+  },
+  {
+    id: 3,
+    img: project3,
+    title: "Quix App",
+    description: "lorem ipsumdolorsit amet"
+  },
+  {
+    id: 4,
+    img: project4,
+    title: "Contact List",
+    description: "COntact list using vue js"
+  }
+]
+
+export const tools = [
+  {
+    id: 1,
+    img: vscodelogo,
+    title: "VS Code",
+    level: "Intermediate",
+  },
+  {
+    id: 2,
+    img: gitlogo,
+    title: "Git",
+    level: "Beginner",
+  },
+  {
+    id: 3,
+    img: githublogo,
+    title: "Github",
+    level: "Beginner",
+  },
+  {
+    id: 4,
+    img: npmlogo,
+    title: "NPM",
+    level: "Beginner"
+  }
+];
+
+export const skills = [
+  {
+    id: 1,
+    img: htmllogo,
+    title: "HTML",
+    level: "Intermediate",
+  },
+  {
+    id: 2,
+    img: csslogo,
+    title: "CSS",
+    level: "Intermediate",
+  },
+  {
+    id: 3,
+    img: jslogo,
+    title: "JavaScript",
+    level: "Intermediate",
+  },
+  {
+    id: 4,
+    img: bootstraplogo,
+    title: "Bootsrap",
+    level: "Beginner",
+  },
+  {
+    id: 5,
+    img: tailwindlogo,
+    title: "TailwindCSS",
+    level: "Intermediate",
+  },
+  {
+    id: 6,
+    img: reactlogo,
+    title: "React.js",
+    level: "Beginner",
+  },
+  {
+    id: 7,
+    img: vuelogo,
+    title: "Vue.js",
+    level: "Beginner",
+  },
+];
+
+export const educations = [
+  {
+    id: 1,
+    instasi: "Universitas Esa Unggul Jakarta",
+    jurusan: "Teknik Informatika",
+    tahun: "2023 - Present",
+  },
+  {
+    id: 2,
+    instasi: "SMA Negeri 79 Jakarta",
+    jurusan: "IPA",
+    tahun: "2020-2023",
+  },
+  {
+    id: 3,
+    instasi: "SMP Negeri 58 Jakarta",
+    jurusan: "Belum ada",
+    tahun: "2017 - 2020",
+  },
+  {
+    id: 4,
+    instasi: "SD Laboratorium PGSD FIP UNJ",
+    jurusan: "Belum Ada",
+    tahun: "2011 - 2017",
+  },
+];
