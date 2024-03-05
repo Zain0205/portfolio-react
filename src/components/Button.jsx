ebaec6af67bc157c44e8f4b52c1bb451dc52abc2
Fahmi Zain <fahmizain000@gmail.com>
2024-03-05 20:24:26 +0700

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,13 @@
-function Button({ children }) {
-  return <button className="px-4 py-2 text-white shadow-lg shadow-secondary border border-primary transition-colors duration-100 ease-out hover:bg-white hover:text-primary bg-primary font-semibold rounded-md">{children}</button>;
+import { motion } from "framer-motion";
+
+function Button({ children, btn, ...props }) {
+  const btnClass = btn ? "px-4 py-2" : "";
+
+  return (
+    <motion.button {...props} className={`text-white ${btnClass} shadow-lg shadow-secondary border border-primary transition-colors duration-100 ease-out hover:bg-white hover:text-primary bg-primary font-semibold rounded-md`}>
+      {children}
+    </motion.button>
+  );
 }
 
 export default Button;
