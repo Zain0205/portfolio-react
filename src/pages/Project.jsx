06d6577764ffb6094ffae44d6bcdcd9e240061e4
Fahmi Zain <fahmizain000@gmail.com>
2024-03-05 20:25:52 +0700

    feat: create show details and add animation

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,30 +1,94 @@
+import { useRef } from "react";
 import Button from "../components/Button";
+import Modal from "../components/Modal";
 import { projects } from "../data";
+import { motion } from "framer-motion";
 
 export default function Project() {
   return (
-    <section id="projects" className="min-h-screen pt-36 pb-16">
-      <div className="px-5 md:px-8 lg:px-36">
-        <h1 className="text-primary font-semibold text-3xl lg:text-4xl">Projects</h1>
-        <div className="flex flex-wrap gap-y-10 gap-x-3 items-center justify-center mt-12">
-        {projects.map(project => <ProjectCard key={project.id} img={project.img} title={project.title} />)}
+    <>
+      <section id="projects" className="pt-32 pb-16">
+        <div className="px-5 md:px-8 lg:px-36">
+          <h1 className="text-primary font-semibold text-3xl lg:text-4xl">Projects</h1>
+          <div className="flex flex-wrap gap-y-10 gap-x-4 items-center justify-center mt-12">
+            {projects.map((project) => (
+              <ProjectCard key={project.id} img={project.img} link={project.link} desc={project.description} title={project.title} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
-function ProjectCard({img, title, description}) {
+function ProjectCard({ img, title, link, desc }) {
+  const showModal = useRef();
+
+  const handleTruncateString = (str, num) => {
+    if (str.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
+
+  const handleShowModal = () => {
+    showModal.current.open();
+  };
+
+  const handleCloseModal = () => {
+    showModal.current.close();
+  };
+
   return (
-    <div className="w-full rounded-lg overflow-hidden shadow-2xl sm:w-[48.9%] lg:w-[49.1%] xl:w-[24.2%]">
-      <div className="">
-        <img src={img} alt={title} />
-      </div>
-      <div className="p-4">
-        <h1 className="text-2xl font-semibold mb-2">{title}</h1>
-        <p className="text-base text-justify mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. A accusantium voluptates maxime consequuntur quas, saepe eos modi iste cum veniam?</p>
-        <Button>See Details</Button>
-      </div>
-    </div>
+    <>
+      <Modal ref={showModal}>
+        <div className="">
+          <img src={img} alt={title} />
+        </div>
+        <div className="p-4">
+          <h1 className="text-2xl font-semibold mb-2">{title}</h1>
+          <p className="text-base mb-4">{desc}</p>
+          <div className="flex gap-x-2 mt-5">
+            <Button onClick={handleCloseModal} btn>
+              Close
+            </Button>
+            <Button>
+              <a className="block px-4 py-2" href={link} target="_blank">
+                See Repository
+              </a>
+            </Button>
+          </div>
+        </div>
+      </Modal>
+      <motion.div
+        initial={{
+          opacity: 0,
+          x: -100,
+          y: 100,
+        }}
+        whileInView={{
+          opacity: 1,
+          x:0,
+          y:0
+        }}
+        transition={{
+          duration: 1,
+          // delay: delay
+        }}
+        className="w-full bg-white rounded-lg overflow-hidden shadow-2xl sm:w-[48.6%] lg:w-[48.8%] xl:w-[23.75%]"
+      >
+        <div className="">
+          <img src={img} alt={title} />
+        </div>
+        <div className="p-4">
+          <h1 className="text-2xl font-semibold mb-2">{title}</h1>
+          <p className="text-base mb-4">{handleTruncateString(desc, 95)}</p>
+          <Button onClick={handleShowModal} btn>
+            See Details
+          </Button>
+        </div>
+      </motion.div>
+    </>
   );
 }
