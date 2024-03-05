6a9e08f1e2d8c59bda83e752d7cc8767e748a703
Fahmi Zain <fahmizain000@gmail.com>
2024-03-05 20:23:30 +0700

    feat: creating modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,56 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { motion, AnimatePresence, useAnimationControls } from "framer-motion";
+
+const Modal = forwardRef(function Modal({ children }, ref) {
+  const modalRef = useRef();
+  const controlModal = useAnimationControls();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        controlModal.start("show");
+        modalRef.current.showModal();
+      },
+      close() {
+        controlModal.start("hide");
+        modalRef.current.close();
+      },
+    };
+  });
+
+  return (
+    <>
+      <AnimatePresence>
+        <motion.dialog
+          variants={{
+            initial: {
+              opacity: 0.5,
+              scale: 0.6,
+            },
+            show: {
+              opacity: 1,
+              scale: 1,
+            },
+            hide: {
+              opacity: 0.5,
+              scale: 0.6,
+            },
+          }}
+          transition={{
+            duration: 0.5,
+            ease: "backOut",
+          }}
+          initial="initial"
+          exit={controlModal}
+          animate={controlModal}
+          className="backdrop:bg-dark/80 rounded-lg overflow-hidden w-full md:w-1/2 lg:w-1/3"
+          ref={modalRef}
+        >
+          {children}
+        </motion.dialog>
+      </AnimatePresence>
+    </>
+  );
+});
+
+export default Modal;
