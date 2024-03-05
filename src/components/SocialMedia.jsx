092306b14e5c30027bc343912314516aab2189f7
Fahmi Zain <fahmizain000@gmail.com>
2024-03-05 20:24:58 +0700

    fix: fixing social media component style

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,7 +1,28 @@
-export default function SocialMedia({ children }) {
+import { motion } from "framer-motion";
+
+export default function SocialMedia({ children, footer, delay }) {
+  const classes = footer ? "border-white text-white shadow-secondary hover:border-dark hover:bg-white hover:shadow-dark" : "shadow-dark border-black text-black hover:border-primary hover:bg-primary";
+
+  const groupHoverClasses = footer ? "group-hover:text-black" : "group-hover:text-white";
+
   return (
-    <div className="mr-3 shadow-md shadow-dark cursor-pointer border border-black hover:border-primary hover:bg-primary p-2 rounded-full group">
-      <div className="text-2xl group-hover:text-white flex items-center justify-center">{children}</div>
-    </div>
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1
+      }}
+      transition={{
+        duration: 1,
+        delay: delay,
+        ease: "easeInOut"
+      }}
+      className={`mr-3 shadow-md cursor-pointer border ${classes} p-2 rounded-full group`}
+    >
+      <div className={`${groupHoverClasses} text-2xl flex items-center justify-center`}>
+        <a href="">{children}</a>
+      </div>
+    </motion.div>
   );
 }
