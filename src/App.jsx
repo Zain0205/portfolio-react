8238b8622310e660ad52a8d315aa4f534373fff3
Fahmi Zain <fahmizain000@gmail.com>
2024-03-06 14:33:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Education from "./pages/Education";
+import Footer from "./pages/Footer";
 import Hero from "./pages/Hero";
+import Project from "./pages/Project";
 import Skills from "./pages/Skills";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Hero />
-    <About />
-    <Education />
-    <Skills />
+      <Navbar />
+      <Hero />
+      <About />
+      <Education />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer /> 
     </>
   );
 }
 
 export default App;
-
