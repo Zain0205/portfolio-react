95eee07ce0dec370e17f2da58a3c5230f28df54f
Fahmi Zain <fahmizain000@gmail.com>
2024-03-06 14:14:33 +0700

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,39 +27,36 @@ export default function Navbar() {
   const border = hamburgerActive ? "border-b border-dark" : "";
   const shadow = shadowNav ? "shadow-sm" : "";
 
-
   return (
-    <header ref={headerRef} className={`shadow-black z-[100] bg-white ${shadow} fixed px-5 ${border} top-0 left-0 w-full flex justify-center items-center md:px-8 lg:px-36`}>
-      <div className="flex justify-between items-center w-full">
-        <div className="w-full">
-          <a href="#" className="font-bold text-xl block py-5 text-dark lg:text-2xl">
-            Fahmi<span className="text-primary">Zain</span>
-          </a>
+    <>
+      <header ref={headerRef} className={`shadow-black z-[100] bg-white ${shadow} fixed px-5 ${border} top-0 left-0 w-full flex justify-center items-center md:px-8 lg:px-36`}>
+        <div className="flex justify-between items-center w-full">
+          <div className="w-full">
+            <a href="#" className="font-bold text-xl block py-5 text-dark lg:text-2xl">
+              Fahmi<span className="text-primary">Zain</span>
+            </a>
+          </div>
+          <div className="flex items-center lg:w-full lg:ml-20">
+            <button onClick={handleHamburger} id="humberger" name="humberger" type="button" className={`block lg:hidden ${activatedHamburger}`}>
+              <span className="w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out origin-top-left"></span>
+              <span className="w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out"></span>
+              <span className="w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out origin-bottom-left"></span>
+            </button>
+            <nav className={`${displayNavigation} absolute bg-primary lg:w-[45vw] shadow-lg w-full right-0 top-[4.3rem] lg:block lg:static lg:bg-transparent lg:shadow-none lg:rounded-none lg:max-w-full`}>
+              <ul className="block lg:justify-between lg:items-center lg:flex w-full">
+                {navbarListItems.map((item) => (
+                  <ListItem onClick={handleNavigationClick} key={item.id} page={item.sectionId}>
+                    {item.list}
+                  </ListItem>
+                ))}
+              </ul>
+            </nav>
+          </div>
         </div>
-        <div className="flex items-center lg:w-full lg:ml-20">
-          <button onClick={handleHamburger} id="humberger" name="humberger" type="button" className={`block lg:hidden ${activatedHamburger}`}>
-            <span className="w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out origin-top-left"></span>
-            <span className="w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out"></span>
-            <span className="w-[30px] h-[2px] my-2 block bg-black transition duration-300 ease-in-out origin-bottom-left"></span>
-          </button>
-          <nav className={`${displayNavigation} absolute bg-primary lg:w-[50vw] shadow-lg w-full right-0 top-[4.3rem] lg:block lg:static lg:bg-transparent lg:shadow-none lg:rounded-none lg:max-w-full`}>
-            <ul className="block lg:justify-between lg:items-center lg:flex w-full">
-              {navbarListItems.map((item) => (
-                <ListItem onClick={handleNavigationClick} key={item.id} page={item.sectionId}>
-                  {item.list}
-                </ListItem>
-              ))}
-              <li className="py-3 lg:py-2 text-white">
-                <button className="bg-white flex px-4 py-2 mx-5 text-primary lg:bg-primary lg:text-white font-semibold rounded-md lg:mx-0">Dark Mode</button>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
-
 
 function ListItem({ children, page, ...props }) {
   return (
